b65149a3688b1dc65777645bce1bee6ece22b9eb
Marin-zzz <darkpatos.com@gmail.com>
2025-05-09 15:45:49 -0400

    Primer commit

---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+    imports: [
+    CommonModule,
+    IonicModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
+  ],
+})
+export class RegisterPage {
+  form: FormGroup;
+  error = '';
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      rut: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      contrasena: ['', Validators.required],
+      tipo: ['alumno', Validators.required], // por defecto "alumno"
+    });
+  }
+
+  async registrar() {
+    if (this.form.invalid) return;
+
+    const { rut, correo, contrasena, tipo } = this.form.value;
+
+    try {
+      await this.authService.register(rut, correo, contrasena, tipo);
+      this.router.navigate(['/login']);
+    } catch (err: any) {
+      this.error = err.message;
+    }
+  }
+}
